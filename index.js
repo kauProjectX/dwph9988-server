9f6017f458f0d89832891f93bfbce6a512f3a116
박준규 <junju404@naver.com>
2024-10-06 19:30:57 +0900

    Feat:: add to dotenv package

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+import "dotenv/config";
 import express from "express";
 import { swaggerUi, swaggerDocs } from "./src/config/swagger/swagger.js";
-const app = express();
-const PORT = process.env.PORT;
 
+const app = express();
+const PORT = process.env.PORT; // 기본 포트 설정 추가
+
+// Swagger 경로 설정
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+// 기본 경로 설정
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+// 서버 시작
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
