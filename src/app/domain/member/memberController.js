448c784f4b1cd6a9277ea4e12fa501982debf0a0
박준규 <junju404@naver.com>
2024-11-09 11:28:52 +0900

    Fix: 일반 로그인에서 카카오 로그인으로 마이그레이션

--- src/app/domain/member/memberController.js
+++ src/app/domain/member/memberController.js
@@ -1,95 +1,116 @@
-import { signupSchema, loginSchema, updateProfileSchema } from './memberDto.js';
 import { MemberService } from './memberService.js';
 import { response } from '../../global/response.js';
 import { BadRequestError } from '../../global/errorHandler.js';
-import { AUTH_CONFIG } from '../../../config/authConfig.js';
-import { UnauthorizedError } from '../../global/errorHandler.js';
 
 export class MemberController {
   constructor() {
     this.memberService = new MemberService();
   }
 
-  signup = async (req, res, next) => {
+  getProfile = async (req, res, next) => {
     try {
-      const { error, value } = signupSchema.validate(req.body);
-      if (error) throw new BadRequestError(error.details[0].message);
+      const user = await this.memberService.getProfile(req.user.id);
+      res.json(
+        response(
+          { isSuccess: true, message: '프로필 조회가 완료되었습니다.' },
+          user
+        )
+      );
+    } catch (err) {
+      next(err);
+    }
+  };
 
-      const { accessToken, refreshToken, user } =
-        await this.memberService.signup(value);
+  requestConnection = async (req, res, next) => {
+    try {
+      const { phoneNumber } = req.body;
+      const verificationData = await this.memberService.requestConnection(
+        req.user.id,
+        phoneNumber
+      );
 
-      res.cookie('refreshToken', refreshToken, AUTH_CONFIG.jwt.cookie);
+      res.json(
+        response(
+          { isSuccess: true, message: '인증번호가 발송되었습니다.' },
+          verificationData
+        )
+      );
+    } catch (err) {
+      next(err);
+    }
+  };
 
-      res
-        .status(201)
-        .json(
+  verifyConnection = async (req, res, next) => {
+    try {
+      const { verificationCode, protectId } = req.body;
+      await this.memberService.verifyConnection(
+        req.user.id,
+        protectId,
+        verificationCode
+      );
+
+      res.json(
+        response({ isSuccess: true, message: '연동이 완료되었습니다.' })
+      );
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  kakaoLogin = async (req, res, next) => {
+    try {
+      if (req.query.access_token) {
+        const userInfo = await this.getKakaoUserInfo(req.query.access_token);
+        const kakaoData = {
+          kakaoId: userInfo.id.toString(),
+          email: userInfo.kakao_account?.email,
+          nickname: userInfo.kakao_account?.profile?.nickname,
+        };
+
+        const { accessToken, user } =
+          await this.memberService.kakaoLogin(kakaoData);
+        return res.json(
           response(
-            { isSuccess: true, message: '회원가입이 완료되었습니다.' },
+            { isSuccess: true, message: '카카오 로그인 성공' },
             { accessToken, user }
           )
         );
-    } catch (err) {
-      next(err);
-    }
-  };
-
-  login = async (req, res, next) => {
-    try {
-      const { error, value } = loginSchema.validate(req.body);
-      if (error) throw new BadRequestError(error.details[0].message);
-
-      const { accessToken, refreshToken, user } =
-        await this.memberService.login(value);
-
-      res.cookie('refreshToken', refreshToken, AUTH_CONFIG.jwt.cookie);
-
-      res.json(
-        response(
-          { isSuccess: true, message: '로그인이 완료되었습니다.' },
-          { accessToken, user }
-        )
-      );
-    } catch (err) {
-      next(err);
-    }
-  };
-
-  updateProfile = async (req, res, next) => {
-    try {
-      const { error, value } = updateProfileSchema.validate(req.body);
-      if (error) throw new BadRequestError(error.details[0].message);
-
-      const updatedUser = await this.memberService.updateProfile(
-        req.user.id,
-        value
-      );
-
-      res.json(
-        response(
-          { isSuccess: true, message: '프로필이 수정되었습니다.' },
-          updatedUser
-        )
-      );
-    } catch (err) {
-      next(err);
-    }
-  };
-
-  refreshToken = async (req, res, next) => {
-    try {
-      const refreshToken = req.cookies.refreshToken;
-      if (!refreshToken) {
-        throw new UnauthorizedError('리프레시 토큰이 없습니다.');
       }
 
-      const { accessToken, user } =
-        await this.memberService.refreshToken(refreshToken);
+      const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_ID}&redirect_uri=${process.env.KAKAO_REDIRECT_URI}&response_type=code`;
+      res.redirect(kakaoAuthURL);
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  logout = async (req, res, next) => {
+    try {
+      const accessToken = req.headers.authorization?.split(' ')[1];
+      await this.memberService.logout(req.user.id, accessToken);
 
       res.json(
-        response(
-          { isSuccess: true, message: '토큰이 갱신되었습니다.' },
-          { accessToken, user }
-        )
+        response({
+          isSuccess: true,
+          message:
+            '로그아웃이 완료되었습니다. 클라이언트에서 토큰을 삭제해주세요.',
+        })
+      );
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  withdraw = async (req, res, next) => {
+    try {
+      const accessToken = req.headers.authorization?.split(' ')[1];
+      await this.memberService.withdraw(req.user.id, accessToken);
+      res.json(
+        response({
+          isSuccess: true,
+          message:
+            '회원 탈퇴가 완료되었습니다. 카카오 계정에서도 로그아웃되었습니다.',
+        })
       );
     } catch (err) {
       next(err);
